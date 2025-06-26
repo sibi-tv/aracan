76f85b7102a925e21cabe676dc6d5c703143fc8d
Sibi Tiruchirapalli <aracan_dev@Charu.localdomain>
2025-06-26 04:17:17 -0400

    project set up done

---
+++ backend/src/main/java/com/aracan/backend/AracanBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.aracan.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AracanBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AracanBackendApiApplication.class, args);
+	}
+
+}

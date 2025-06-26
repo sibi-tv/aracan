8f849224bcdc7190e999624472510d0981c2179b
Sibi Tiruchirapalli <aracan_dev@Charu.localdomain>
2025-06-26 12:01:23 -0400

    hehe

--- backend/src/main/java/com/aracan/backend/entity/Order.java
+++ backend/src/main/java/com/aracan/backend/entity/Order.java
@@ -1,2 +1,13 @@
-package com.aracan.backend.entity;public class Order {
+package com.aracan.backend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "Users")
+@Data
+@NoArgsConstructor
+public class Order {
 }
